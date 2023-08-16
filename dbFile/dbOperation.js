d44dafc348002fb55be5e2ce1da088deb0708592
Giorgi Rizhamadze <g.rizhamadze@crystal.ge>
2023-08-16 16:04:34 +0400

    get post request

--- dbFile/dbOperation.js
+++ dbFile/dbOperation.js
@@ -19,14 +19,14 @@ const getClosedLoans=async (personalNo,agreementNo)=>{
 
 const updateClosedLoans=async (closedLoan)=>{
     closedLoan.savedData.map(async loan=>{
-        console.log('closedLoan ',loan);
+        console.log('closedLoan ',loan.LoanId);
         try {
             let pool = await sql.connect(config);
-            let closedLoansk = pool.request().query(`UPDATE [CrystalDB].[dbo].[KEEPER_ClosedLoans] SET [Status] = N'${loan.status}',[packN]='${loan.packN}' ,[boxN]='${loan.boxN || ''}' WHERE LoanId=${loan.id}`)
-            let closedLoansL = pool.request().query(`UPDATE [CrystalDB].[dbo].[LMS_ClosedLoans] SET [Status] = N'${loan.status}',[packN]='${loan.packN}' ,[boxN]='${loan.boxN || ''}' WHERE LoanId=${loan.id}`)
-            return closedLoansk , closedLoansL
+            let closedLoansk = pool.request().query(`UPDATE [CrystalDB].[dbo].[KEEPER_ClosedLoans] SET [Status] = N'${loan.Status}',[packN]='${loan.packN}' ,[boxN]='${loan.boxN || ''}' WHERE LoanId=${loan.LoanId}`)
+            let closedLoansL = pool.request().query(`UPDATE [CrystalDB].[dbo].[LMS_ClosedLoans] SET [Status] = N'${loan.Status}',[packN]='${loan.packN}' ,[boxN]='${loan.boxN || ''}' WHERE LoanId=${loan.LoanId}`)
+             return closedLoansk , closedLoansL
         } catch(error) {
-            console.log(error);  
+            console.log('err ',error);  
         }
     })
    
